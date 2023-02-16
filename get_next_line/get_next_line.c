f710b6217e80aa763b2e99012619f62913b165d5
lvelasqu <lsbvc95@gmail.com>
2023-02-16 19:36:20 +0100

    get_next_line 85%

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,27 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lvelasqu <lvelasqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/08 14:12:00 by lvelasqu          #+#    #+#             */
-/*   Updated: 2023/02/08 14:12:08 by lvelasqu         ###   ########.fr       */
+/*   Created: 2023/01/19 12:32:59 by lvelasqu          #+#    #+#             */
+/*   Updated: 2023/02/15 12:15:41 by lvelasqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	ft_line(char *buffer)
-{
-	
-}
-
-char	ft_read_file(char *buffer, char *mini_buffer)
+// join and free
+char	*ft_free(char *buffer, char *buf)
 {
 	char	*temp;
 
-	temp = ft_strjoin(buffer, mini_buffer);
+	temp = ft_strjoin(buffer, buf);
 	free(buffer);
 	return (temp);
 }
 
+// delete line find
 char	*ft_next(char *buffer)
 {
 	int		i;
@@ -33,20 +30,84 @@ char	*ft_next(char *buffer)
 	char	*line;
 
 	i = 0;
+	// find len of first line
 	while (buffer[i] && buffer[i] != '\n')
 		i++;
+	// if eol == \0 return NULL
 	if (!buffer[i])
 	{
 		free(buffer);
 		return (NULL);
 	}
+	// len of file - len of firstline + 1
 	line = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
 	i++;
 	j = 0;
+	// line == buffer
 	while (buffer[i])
 		line[j++] = buffer[i++];
 	free(buffer);
 	return (line);
+}
+
+// take line for return
+char	*ft_line(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	// if no line return NULL
+	if (!buffer[i])
+		return (NULL);
+	// go to the eol
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	// malloc to eol
+	line = ft_calloc(i + 2, sizeof(char));
+	i = 0;
+	// line = buffer
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	// if eol is \0 or \n, replace eol by \n
+	if (buffer[i] && buffer[i] == '\n')
+		line[i++] = '\n';
+	return (line);
+}
+
+char	*read_file(int fd, char *res)
+{
+	char	*buffer;
+	int		byte_read;
+
+	// malloc if res dont exist
+	if (!res)
+		res = ft_calloc(1, 1);
+	// malloc buffer
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	byte_read = 1;
+	while (byte_read > 0)
+	{
+		// while not eof read
+		byte_read = read(fd, buffer, BUFFER_SIZE);
+		if (byte_read == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		// 0 to end for leak
+		buffer[byte_read] = 0;
+		// join and free
+		res = ft_free(res, buffer);
+		// quit if \n find
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	free(buffer);
+	return (res);
 }
 
 char	*get_next_line(int fd)
@@ -54,9 +115,10 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0))
+	// error handling
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
-	buffer = ft_read_file(fd, buffer);
+	buffer = read_file(fd, buffer);
 	if (!buffer)
 		return (NULL);
 	line = ft_line(buffer);
@@ -64,31 +126,22 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-/*int	main(void)
+int main(void)
 {
-	int			fd;
-	char		*buf;
-	ssize_t		nr_bytes;
+char *line;
+int fd = open("example.txt", O_RDONLY);
+if (fd == -1)
+{
+printf("Failed to open file.\n");
+return 1;
+}
 
-	buf = (char *) malloc (sizeof(char) * 16);
-	fd = open("/Users/lvelasqu/Proyects/lvelasqu/get_next_line/file", O_RDONLY);
-	if (fd == -1)
-	{
-		printf("Error while opening file \n");
-	}
-	else
-	{
-		nr_bytes = read(fd, buf, 3);
-		close(fd);
-		if (nr_bytes == 0)
-		{
-			printf("Empty file \n");
-		}
-		else
-		{
-			printf("The number of characters is: %d, the content is: %s", (int)nr_bytes, buf);
-		}
-	}
-	free (buf);
-	return (0);
-}*/
+while ((line = get_next_line(fd)))
+{
+printf("%s", line);
+free(line);
+}
+
+close(fd);
+return 0;
+}
